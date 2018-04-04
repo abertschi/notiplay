65763d2ebcd2d673a0d88a36a3931d456630befa
Andrin Bertschi <andrin.bertschi@gmail.com>
2018-04-04 16:11:04 +0200

    wip

--- app/src/main/java/ch/abertschi/notiplay/HomeActivity.kt
+++ app/src/main/java/ch/abertschi/notiplay/HomeActivity.kt
@@ -1,7 +1,6 @@
 package ch.abertschi.notiplay
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 
 /**
@@ -9,8 +8,8 @@ import android.support.v7.app.AppCompatActivity
  */
 class HomeActivity: AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
     }
 
